798ddeb357f198cc546a5b97923a13d2dd1c165b
Griffin Hadfield <griffhadfield@gmail.com>
2021-03-09 14:30:35 -0500

    enabled PR tests

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -16,6 +16,6 @@ cat inventory/pom.xml system/pom.xml
 sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" inventory/Dockerfile system/Dockerfile
 cat inventory/Dockerfile system/Dockerfile
 
-sudo ../scripts/startMinikube.sh
+docker pull $DOCKER_USERNAME"/olguides:"$BUILD
+
 sudo ../scripts/testApp.sh
-sudo ../scripts/stopMinikube.sh
--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
-##############################################################################
-##
-##  GH actions CI test script
-##
-##############################################################################
+# Package the system/ and inventory/ apps
+mvn -q -pl models install
+mvn -q clean package
 
-mvn -q package
+# Verifies that the system/inventory apps are functional
+mvn -pl system verify
+mvn -pl inventory verify
 
-docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
-
-docker build -t system:test system/.
-docker build -t inventory:test inventory/.
-
-kubectl apply -f ../scripts/test.yaml
-
-sleep 120
-
-kubectl get pods
-
-GUIDE_IP=$(minikube ip)
-GUIDE_SYSTEM_PORT=$(kubectl get service system-service -o jsonpath="{.spec.ports[0].nodePort}")
-GUIDE_INVENTORY_PORT=$(kubectl get service inventory-service -o jsonpath="{.spec.ports[0].nodePort}")
-
-curl http://$GUIDE_IP:$GUIDE_SYSTEM_PORT/system/properties
-
-curl http://$GUIDE_IP:$GUIDE_INVENTORY_PORT/inventory/systems/system-service
-
-SYSTEM_IP=$GUIDE_IP:$GUIDE_SYSTEM_PORT
-INVENTORY_IP=$GUIDE_IP:$GUIDE_INVENTORY_PORT
-
-mvn verify -Ddockerfile.skip=true -Dsystem.ip=$SYSTEM_IP -Dinventory.ip=$INVENTORY_IP
-
-kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)
-kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)
+# Delete m2 cache after completion
+rm -rf ~/.m2
