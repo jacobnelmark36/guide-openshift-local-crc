d220ba49ed8458bfe4786d312001de4a93872750
Griffin Hadfield <griffhadfield@gmail.com>
2021-03-15 15:01:30 -0400

    enable pr tests

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,13 +1,36 @@
-#!/bin/bash
 set -euxo pipefail
 
-# Package the system/ and inventory/ apps
-mvn -q -pl models install
-mvn -q clean package
+##############################################################################
+##
+##  GH actions CI test script
+##
+##############################################################################
 
-# Verifies that the system/inventory apps are functional
-mvn -pl system verify
-mvn -pl inventory verify
+mvn -q package
 
-# Delete m2 cache after completion
-rm -rf ~/.m2
+docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
+
+docker build -t system:test system/.
+docker build -t inventory:test inventory/.
+
+kubectl apply -f ../scripts/test.yaml
+
+sleep 120
+
+kubectl get pods
+
+GUIDE_IP=$(minikube ip)
+GUIDE_SYSTEM_PORT=$(kubectl get service system-service -o jsonpath="{.spec.ports[0].nodePort}")
+GUIDE_INVENTORY_PORT=$(kubectl get service inventory-service -o jsonpath="{.spec.ports[0].nodePort}")
+
+curl http://$GUIDE_IP:$GUIDE_SYSTEM_PORT/system/properties
+
+curl http://$GUIDE_IP:$GUIDE_INVENTORY_PORT/inventory/systems/system-service
+
+SYSTEM_IP=$GUIDE_IP:$GUIDE_SYSTEM_PORT
+INVENTORY_IP=$GUIDE_IP:$GUIDE_INVENTORY_PORT
+
+mvn verify -Ddockerfile.skip=true -Dsystem.ip=$SYSTEM_IP -Dinventory.ip=$INVENTORY_IP
+
+kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)
+kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)
