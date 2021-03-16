93aadca1c2201321cba43231452933e14dc1e1ca
Griffin Hadfield <griffhadfield@gmail.com>
2021-03-16 16:12:53 -0400

    update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -12,10 +12,6 @@ export HOSTNAME=localhost
 
 
 ## Rebuild the application
-#       package                   - Take the compiled code and package it in its distributable format.
-#       liberty:create            - Create a Liberty server.
-#       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
-#       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
 mvn -q clean package 
 mvn -pl system liberty:create 
 mvn -pl system liberty:install-feature 
@@ -25,16 +21,9 @@ mvn -pl inventory liberty:create
 mvn -pl inventory liberty:install-feature 
 mvn -pl inventory liberty:deploy
 
-
 ## Run the tests
-# These commands are separated because if one of the commands fail, the test script will fail and exit.
-# e.g if liberty:start fails, then there is no need to run the failsafe commands.
-#       liberty:start             - Start a Liberty server in the background.
-#       failsafe:integration-test - Runs the integration tests of an application.
-#       liberty:stop              - Stop a Liberty server.
-#       failsafe:verify           - Verifies that the integration tests of an application passed.
 mvn -pl system liberty:start
 mvn -pl inventory liberty:start
-mvn verify -Dsystem.ip=localhost:9080 -Dinventory.ip=localhost:8080 liberty:stop
-# mvn failsafe:integration-test 
-# mvn failsafe:verify
+mvn verify -Dsystem.ip=localhost:9080 -Dinventory.ip=localhost:8080 
+mvn -pl system liberty:stop 
+mvn -pl inventory liberty:stop
